ddf68c2e980c8d7ca628f69720862efc7585ea9d
RootCellar <RootCellar9877@GMail.com>
2020-12-07 20:50:57 -0900

    Begin Development

---
+++ src/VirtualComputer.java
@@ -0,0 +1,18 @@
+/*
+ * RootCellar (github.com/rootcellar)
+ * 12/07/2020
+ * Creating virtual hardware in hopes of emulating a machine
+ *
+ *
+*/
+
+
+public class VirtualComputer {
+
+  
+
+  public VirtualComputer() {
+
+  }
+
+}
