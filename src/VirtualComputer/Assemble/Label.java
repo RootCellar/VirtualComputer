a3c68a3cde6ad97899e464f4f0a7d74658f4a833
RootCellar <RootCellar9877@GMail.com>
2021-03-12 20:53:37 -0900

    implement class Label

---
+++ src/VirtualComputer/Assemble/Label.java
@@ -0,0 +1,17 @@
+package VirtualComputer.Assemble;
+
+public class Label {
+
+  //Private Data
+  private String name;
+  private int position;
+
+  public Label(String n, int pos) {
+    name = n;
+    position = pos;
+  }
+
+  public String getName() { return name; }
+  public int getPos() { return position; }
+
+}
