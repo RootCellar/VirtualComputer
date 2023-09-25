74c1fee6518dc67198f66595aab4cd689830c53f
RootCellar <RootCellar9877@gmail.com>
2023-09-25 15:47:42 -0800

    replace with actual value for clarity

--- src/VirtualComputer/Assembler.java
+++ src/VirtualComputer/Assembler.java
@@ -628,7 +628,7 @@ public class Assembler {
         //Empty
         if( parts.length < 1 || parts[0].isEmpty() ) {
             debug("Empty line");
-            return instr;
+            return null;
         }
 
         boolean made = false;
